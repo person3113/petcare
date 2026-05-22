de92e36e9b2ce067dd8c2f3c874587639e4610ea
Beaeze <ddong9275@naver.com>
2026-05-22 15:26:53 +0900

    feat: 동물 스와이프 페이지 구현 #18

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout.jsx';
 import AnimalDetailpage from './pages/AnimalDetailpage.jsx';
+import AnimalSwipepage from "./pages/AnimalSwipepage.jsx";
 import MatchResult from './pages/MatchResult.jsx';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
@@ -16,6 +17,7 @@ function App() {
                 {/* 여기에 layout파일을 상속받는 페이지들을 넣으면 됨*/}
                 <Route index element={<Home />} />
                 <Route path="animal/:id" element={<AnimalDetailpage />} /> {/*동물 상세페이지*/}
+                <Route path="/animalswipe/:id" element={<AnimalSwipepage />} />{/*동물 스와이프페이지*/}
                 <Route path="survey" element={<Survey />} />
                 <Route path="match-result" element={<MatchResult />} />
                 <Route path="login" element={<Login />} />
