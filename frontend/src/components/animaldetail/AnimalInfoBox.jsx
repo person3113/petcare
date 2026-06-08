e717a839f14e2cbb42a7d167f888fd64b41413af
Beaeze <ddong9275@naver.com>
2026-06-08 15:48:22 +0900

    feat:동물 상세 페이지 피드백디자인 반영

--- frontend/src/components/animaldetail/AnimalInfoBox.jsx
+++ frontend/src/components/animaldetail/AnimalInfoBox.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { addFavorite, removeFavorite } from '../../api/favorites.js';
 import Tag from '../Tag';
 import Button from '../Button';
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { LuMessageSquareHeart } from "react-icons/lu";
 
 function AnimalInfoBox({animal, onFavoriteChange}) {
 
@@ -18,9 +20,11 @@ function AnimalInfoBox({animal, onFavoriteChange}) {
         healthStatus,  //건강상태
         discoveryDate, //발견날짜
         discoveryPlace, //발견장소
-        isLiked,
+        liked,
         geminiIntro,
     } = animal;
+
+    console.log(animal)
 
     const [favoriteLoading, setFavoriteLoading] = useState(false);
 
@@ -34,12 +38,14 @@ function AnimalInfoBox({animal, onFavoriteChange}) {
         }
         setFavoriteLoading(true);
         try {
-            if (isLiked) {
+            if (liked) {
                 await removeFavorite(animal.id);
+                console.log('찜하기 삭제 성공');
             } else {
                 await addFavorite(animal.id);
+                console.log('찜하기 추가 성공');
             }
-            const updated = { ...animal, isLiked: !isLiked };
+            const updated = { ...animal, liked: !liked };
             onFavoriteChange?.(updated);
         } catch (error) {
             console.log('찜하기 처리 실패', error);
@@ -54,7 +60,7 @@ function AnimalInfoBox({animal, onFavoriteChange}) {
 
             {/*타이틀: 품종 및 보호 상태 */}
             <div className="flex items-center gap-3">
-                <h1 className="!text-4xl font-bold">
+                <h1 className="!text-4xl font-bold m-0">
                     {kind} {/* ex) [개] 믹스견 */}
                 </h1>
                 <span className="rounded bg-statusbg px-2 py-1 text-sm font-medium text-accent">
@@ -63,22 +69,27 @@ function AnimalInfoBox({animal, onFavoriteChange}) {
             </div>
 
 
-            {/*종 · 나이 · 성별 · 체중 · 보호소명*/}
-            <h2>기본 정보</h2>
-            <div className="pt-6 text-sm text-gray-600">
-                <p>종: {kind?.replace(/\[.*\\]\s*/, '')}</p>
-                <p>나이: {age}</p>
-                <p>성별: {gender}</p>
-                <p>무게: {weight}</p>
-                <p>보호소: {shelterName}</p>
-                <p>발견 날짜: {discoveryDate}</p>
-                <p>발견 장소: {discoveryPlace}</p>
+            <div className="mt-1 rounded-xl border border-gray-100 bg-white p-6 text-sm text-gray-700 shadow-sm">
+                {/*종 · 나이 · 성별 · 체중 · 보호소명*/}
+                <h2 className="!text-accent">기본 정보</h2>
+                <hr className="text-accent"></hr>
+                <div className="pt-6 text-sm text-gray-600">
+                    <p className="flex"><span className="w-25">종</span> <span>{kind?.replace(/\[.*\\]\s*/, '')}</span></p>
+                    <p className="flex"><span className="w-25">나이</span> <span>{age}</span></p>
+                    <p className="flex"><span className="w-25">성별</span> <span>{gender}</span></p>
+                    <p className="flex"><span className="w-25">무게</span> <span>{weight}</span></p>
+                    <p className="flex"><span className="w-25">보호소</span> <span>{shelterName}</span></p>
+                    <p className="flex"><span className="w-25">발견 날짜</span> <span>{discoveryDate}</span></p>
+                    <p className="flex"><span className="w-25">발견 장소</span> <span>{discoveryPlace}</span></p>
+                </div>
             </div>
 
-            {/*AI 1줄 소개*/}
-            <h2>AI 1줄 소개</h2>
-            <div className="mt-4 text-sm text-gray-700">
-                <p>{geminiIntro || '이 아이의 소개글을 준비 중이에요.'}</p>
+            <div className="bg-statusbg p-4 rounded-lg m-2">
+                {/*AI 1줄 소개*/}
+                <h2 className="!text-accent flex gap-2">AI 1줄 소개 <span><LuMessageSquareHeart className="text-accent" size={24} /></span></h2>
+                <div className="mt-4 text-sm text-gray-700">
+                    <p>{geminiIntro || '이 아이의 소개글을 준비 중이에요.'}</p>
+                </div>
             </div>
 
             {/* 태그 부분: 데이터에 배열 형식의 태그가 없으므로, 핵심 정보를 태그 형태로 시각화 */}
@@ -91,9 +102,10 @@ function AnimalInfoBox({animal, onFavoriteChange}) {
             {/* 버튼 영역 */}
             <div className="mt-6 flex flex-wrap items-center gap-3">
                 <Button
-                    text={favoriteLoading ? "..." : (isLiked ? "❤️" : "🤍")}
+                    text={favoriteLoading ? "..." : (liked ?  <FaHeart size={24}/>:<FaRegHeart size={24}/>)}
                     flex={0.4}
                     bgColor="none" // 원하면 배경 추가
+                    textColor="#ef4444"
                     onClick={handleToggleFavorite}
                 />
                 <Button
